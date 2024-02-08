2b42ccda97a3ab0dedc26938378ff2b1c6b8d65c
Francisco Roa <franroav@gmail.com>
2024-02-07 22:33:21 -0300

    Network Layout - Multi-tier Architecture - update secrets values

--- scripts/env/production/terraform.sh
+++ scripts/env/production/terraform.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-set -e
 
-# Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Navigate to the root directory of the project
-ROOT_DIR="$(dirname "$SCRIPT_DIR")"
-
-# Check if .env file exists in the root directory
-if [ -f "$ROOT_DIR/.env" ]; then
-    # If the .env file exists, load the variables
-    source "$ROOT_DIR/.env"
-else
-    # If .env file doesn't exist, print an error message and exit
-    echo "Error: .env file not found in the root directory."
-    exit 1
-fi
 
 # Install necessary dependencies
 sudo apt-get update
@@ -35,48 +19,6 @@ terraform init
 
 # Validate Terraform configuration
 terraform validate
-
-# Plan Terraform changes
-# terraform plan \
-#   -var "TF_VAR_NAME=$TF_VAR_NAME" \
-#   -var "TF_VAR_ENV=$TF_VAR_ENV" \
-#   -var "TF_VAR_VPC_CIDR=$TF_VAR_VPC_CIDR" \
-#   -var "TF_VAR_PUBLIC_SUBNETS_CIDR=$TF_VAR_PUBLIC_SUBNETS_CIDR" \
-#   -var "TF_VAR_PRIVATE_SUBNETS_CIDR=$TF_VAR_PRIVATE_SUBNETS_CIDR" \
-#   -var "TF_VAR_AVAILABILITY_ZONES=$TF_VAR_AVAILABILITY_ZONES" \
-#   -var "TF_VAR_EC2_AMI=$TF_VAR_EC2_AMI" \
-#   -var "TF_VAR_EC2_INSTANCE_TYPE=$TF_VAR_EC2_INSTANCE_TYPE" \
-#   -var "TF_VAR_AWS_ACCESS_KEY_ID=$TF_VAR_AWS_ACCESS_KEY_ID" \
-#   -var "TF_VAR_AWS_SECRET_ACCESS_KEY=$TF_VAR_AWS_SECRET_ACCESS_KEY" \
-#   -var "TF_VAR_AWS_SESSION_TOKEN=$TF_VAR_AWS_SESSION_TOKEN" \
-#   -var "TF_VAR_AWS_ACCOUNT_ID=$TF_VAR_AWS_ACCOUNT_ID" \
-#   -var "TF_VAR_AWS_REGION=$TF_VAR_AWS_REGION" \
-#   -var "TF_VAR_AWS_PROFILE=$TF_VAR_AWS_PROFILE" \
-#   -var "TF_VAR_ENABLE_VPN_GATEWAY=$TF_VAR_ENABLE_VPN_GATEWAY" \
-#   -var "TF_VAR_ENABLE_DNS_HOSTNAMES=$TF_VAR_ENABLE_DNS_HOSTNAMES" \
-#   -var "TF_VAR_ENABLE_DNS_SUPPORT=$TF_VAR_ENABLE_DNS_SUPPORT" \
-#   -var "TF_VAR_TERRAFORM=$TF_VAR_TERRAFORM"
-
-# # Apply Terraform changes
-# terraform apply -auto-approve \
-#   -var "TF_VAR_NAME=${{ secrets.TF_VAR_NAME }}" \
-#   -var "TF_VAR_ENV=${{ secrets.TF_VAR_ENV }}" \
-#   -var "TF_VAR_VPC_CIDR=${{ secrets.TF_VAR_VPC_CIDR }}" \
-#   -var "TF_VAR_PUBLIC_SUBNETS_CIDR=${{ secrets.TF_VAR_PUBLIC_SUBNETS_CIDR }}" \
-#   -var "TF_VAR_PRIVATE_SUBNETS_CIDR=${{ secrets.TF_VAR_PRIVATE_SUBNETS_CIDR }}" \
-#   -var "TF_VAR_AVAILABILITY_ZONES=${{ secrets.TF_VAR_AVAILABILITY_ZONES }}" \
-#   -var "TF_VAR_EC2_AMI=${{ secrets.TF_VAR_EC2_AMI }}" \
-#   -var "TF_VAR_EC2_INSTANCE_TYPE=${{ secrets.TF_VAR_EC2_INSTANCE_TYPE }}" \
-#   -var "TF_VAR_ENABLE_VPN_GATEWAY=${{ secrets.TF_VAR_ENABLE_VPN_GATEWAY }}" \
-#   -var "TF_VAR_ENABLE_DNS_HOSTNAMES=${{ secrets.TF_VAR_ENABLE_DNS_HOSTNAMES }}" \
-#   -var "TF_VAR_ENABLE_DNS_SUPPORT=${{ secrets.TF_VAR_ENABLE_DNS_SUPPORT }}" \
-#   -var "TF_VAR_TERRAFORM=${{ secrets.TF_VAR_TERRAFORM }}" \
-#   -var "TF_VAR_AWS_ACCOUNT_ID=${{ secrets.TF_VAR_AWS_ACCOUNT_ID }}" \
-#   -var "TF_VAR_AWS_REGION=${{ secrets.TF_VAR_AWS_REGION }}" \
-#   -var "TF_VAR_AWS_PROFILE=${{ secrets.TF_VAR_AWS_PROFILE }}" \
-#   -var "TF_VAR_AWS_ACCESS_KEY_ID=${{ secrets.TF_VAR_AWS_ACCESS_KEY_ID }}" \
-#   -var "TF_VAR_AWS_SECRET_ACCESS_KEY=${{ secrets.TF_VAR_AWS_SECRET_ACCESS_KEY }}" \
-#   -var "TF_VAR_AWS_SESSION_TOKEN=${{ secrets.TF_VAR_AWS_SESSION_TOKEN }}"
 
 terraform plan \
   -var "TF_VAR_NAME=$TF_VAR_NAME" \
@@ -123,6 +65,49 @@ terraform apply \
 #Destroy Terraform changes with auto-approval and passing variables
   terraform destroy \
   -auto-approve \
+
+
+# Plan Terraform changes
+# terraform plan \
+#   -var "TF_VAR_NAME=$TF_VAR_NAME" \
+#   -var "TF_VAR_ENV=$TF_VAR_ENV" \
+#   -var "TF_VAR_VPC_CIDR=$TF_VAR_VPC_CIDR" \
+#   -var "TF_VAR_PUBLIC_SUBNETS_CIDR=$TF_VAR_PUBLIC_SUBNETS_CIDR" \
+#   -var "TF_VAR_PRIVATE_SUBNETS_CIDR=$TF_VAR_PRIVATE_SUBNETS_CIDR" \
+#   -var "TF_VAR_AVAILABILITY_ZONES=$TF_VAR_AVAILABILITY_ZONES" \
+#   -var "TF_VAR_EC2_AMI=$TF_VAR_EC2_AMI" \
+#   -var "TF_VAR_EC2_INSTANCE_TYPE=$TF_VAR_EC2_INSTANCE_TYPE" \
+#   -var "TF_VAR_AWS_ACCESS_KEY_ID=$TF_VAR_AWS_ACCESS_KEY_ID" \
+#   -var "TF_VAR_AWS_SECRET_ACCESS_KEY=$TF_VAR_AWS_SECRET_ACCESS_KEY" \
+#   -var "TF_VAR_AWS_SESSION_TOKEN=$TF_VAR_AWS_SESSION_TOKEN" \
+#   -var "TF_VAR_AWS_ACCOUNT_ID=$TF_VAR_AWS_ACCOUNT_ID" \
+#   -var "TF_VAR_AWS_REGION=$TF_VAR_AWS_REGION" \
+#   -var "TF_VAR_AWS_PROFILE=$TF_VAR_AWS_PROFILE" \
+#   -var "TF_VAR_ENABLE_VPN_GATEWAY=$TF_VAR_ENABLE_VPN_GATEWAY" \
+#   -var "TF_VAR_ENABLE_DNS_HOSTNAMES=$TF_VAR_ENABLE_DNS_HOSTNAMES" \
+#   -var "TF_VAR_ENABLE_DNS_SUPPORT=$TF_VAR_ENABLE_DNS_SUPPORT" \
+#   -var "TF_VAR_TERRAFORM=$TF_VAR_TERRAFORM"
+
+# # Apply Terraform changes
+# terraform apply -auto-approve \
+#   -var "TF_VAR_NAME=${{ secrets.TF_VAR_NAME }}" \
+#   -var "TF_VAR_ENV=${{ secrets.TF_VAR_ENV }}" \
+#   -var "TF_VAR_VPC_CIDR=${{ secrets.TF_VAR_VPC_CIDR }}" \
+#   -var "TF_VAR_PUBLIC_SUBNETS_CIDR=${{ secrets.TF_VAR_PUBLIC_SUBNETS_CIDR }}" \
+#   -var "TF_VAR_PRIVATE_SUBNETS_CIDR=${{ secrets.TF_VAR_PRIVATE_SUBNETS_CIDR }}" \
+#   -var "TF_VAR_AVAILABILITY_ZONES=${{ secrets.TF_VAR_AVAILABILITY_ZONES }}" \
+#   -var "TF_VAR_EC2_AMI=${{ secrets.TF_VAR_EC2_AMI }}" \
+#   -var "TF_VAR_EC2_INSTANCE_TYPE=${{ secrets.TF_VAR_EC2_INSTANCE_TYPE }}" \
+#   -var "TF_VAR_ENABLE_VPN_GATEWAY=${{ secrets.TF_VAR_ENABLE_VPN_GATEWAY }}" \
+#   -var "TF_VAR_ENABLE_DNS_HOSTNAMES=${{ secrets.TF_VAR_ENABLE_DNS_HOSTNAMES }}" \
+#   -var "TF_VAR_ENABLE_DNS_SUPPORT=${{ secrets.TF_VAR_ENABLE_DNS_SUPPORT }}" \
+#   -var "TF_VAR_TERRAFORM=${{ secrets.TF_VAR_TERRAFORM }}" \
+#   -var "TF_VAR_AWS_ACCOUNT_ID=${{ secrets.TF_VAR_AWS_ACCOUNT_ID }}" \
+#   -var "TF_VAR_AWS_REGION=${{ secrets.TF_VAR_AWS_REGION }}" \
+#   -var "TF_VAR_AWS_PROFILE=${{ secrets.TF_VAR_AWS_PROFILE }}" \
+#   -var "TF_VAR_AWS_ACCESS_KEY_ID=${{ secrets.TF_VAR_AWS_ACCESS_KEY_ID }}" \
+#   -var "TF_VAR_AWS_SECRET_ACCESS_KEY=${{ secrets.TF_VAR_AWS_SECRET_ACCESS_KEY }}" \
+#   -var "TF_VAR_AWS_SESSION_TOKEN=${{ secrets.TF_VAR_AWS_SESSION_TOKEN }}"
 
 # set -e
 
